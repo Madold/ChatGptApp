02d296c6244a451a7cda284d50e6dc41bee52f71
Marco Portacio <marcoportacio4@gmail.com>
2023-04-08 14:54:33 -0500

    The UI content is now set by a fragment and not by the MainActivity

--- app/src/main/java/com/markusw/chatgptapp/ui/view/activities/main/MainActivity.kt
+++ app/src/main/java/com/markusw/chatgptapp/ui/view/activities/main/MainActivity.kt
@@ -1,74 +1,18 @@
-@file:OptIn(ExperimentalComposeUiApi::class)
-
 package com.markusw.chatgptapp.ui.view.activities.main
 
 import android.os.Bundle
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.runtime.SideEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.ExperimentalComposeUiApi
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
-import com.markusw.chatgptapp.BuildConfig
 import com.markusw.chatgptapp.databinding.ActivityMainBinding
-import com.markusw.chatgptapp.ui.theme.ChatGptAppTheme
-import com.markusw.chatgptapp.ui.view.screens.main.MainScreen
-import com.markusw.chatgptapp.ui.viewmodel.main.MainScreenViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private val viewModel: MainScreenViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        binding.composeView.setContent {
-
-            val state by viewModel.uiState.collectAsStateWithLifecycle()
-            val focusManager = LocalFocusManager.current
-
-            ChatGptAppTheme(
-                dynamicColor = false,
-            ) {
-
-                val systemUiController = rememberSystemUiController()
-                val isSystemInDarkTheme = isSystemInDarkTheme()
-                val systemBarsColors = MaterialTheme.colorScheme.background
-
-                SideEffect {
-                    systemUiController.setSystemBarsColor(
-                      color =  systemBarsColors,
-                        darkIcons = !isSystemInDarkTheme
-                    )
-                }
-
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    MainScreen(
-                        state = state,
-                        onSendButtonClick = {
-                            viewModel.onPromptSend()
-                            focusManager.clearFocus()
-                        },
-                        onPromptChanged = viewModel::onPromptChanged
-                    )
-                }
-            }
-        }
     }
 }
--- app/src/main/java/com/markusw/chatgptapp/ui/view/fragments/MainScreenFragment.kt
+++ app/src/main/java/com/markusw/chatgptapp/ui/view/fragments/MainScreenFragment.kt
@@ -4,21 +4,27 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import com.markusw.chatgptapp.ui.theme.ChatGptAppTheme
 import com.markusw.chatgptapp.ui.view.screens.main.MainScreen
 import com.markusw.chatgptapp.ui.viewmodel.main.MainScreenViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class ChatScreenFragment: Fragment() {
+class MainScreenFragment : Fragment() {
 
     private lateinit var composeView: ComposeView
     private val viewModel: MainScreenViewModel by viewModels()
@@ -34,16 +40,36 @@ class ChatScreenFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        composeView.setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnLifecycleDestroyed(viewLifecycleOwner))
         composeView.setContent {
             val state by viewModel.uiState.collectAsStateWithLifecycle()
-            MaterialTheme {
+            val focusManager = LocalFocusManager.current
+
+            ChatGptAppTheme(
+                dynamicColor = false,
+            ) {
+
+                val systemUiController = rememberSystemUiController()
+                val isSystemInDarkTheme = isSystemInDarkTheme()
+                val systemBarsColors = MaterialTheme.colorScheme.background
+
+                SideEffect {
+                    systemUiController.setSystemBarsColor(
+                        color = systemBarsColors,
+                        darkIcons = !isSystemInDarkTheme
+                    )
+                }
+
                 Surface(
-                    modifier = Modifier.fillMaxSize()
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
                 ) {
                     MainScreen(
                         state = state,
-                        onSendButtonClick = viewModel::onPromptSend,
+                        onSendButtonClick = {
+                            viewModel.onPromptSend()
+                            focusManager.clearFocus()
+                        },
                         onPromptChanged = viewModel::onPromptChanged
                     )
                 }
