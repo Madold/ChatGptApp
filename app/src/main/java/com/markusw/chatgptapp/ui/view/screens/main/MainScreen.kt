f0411545ef931fb0a6568902a008a43b4c98cdd7
Marco Portacio <marcoportacio4@gmail.com>
2023-04-11 09:35:08 -0500

    Optimized imports for MainScreen.kt file

--- app/src/main/java/com/markusw/chatgptapp/ui/view/screens/main/MainScreen.kt
+++ app/src/main/java/com/markusw/chatgptapp/ui/view/screens/main/MainScreen.kt
@@ -12,15 +12,12 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.SideEffect
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.markusw.chatgptapp.ui.view.screens.main.composables.ChatItem
 import com.markusw.chatgptapp.ui.view.screens.main.composables.MainScreenTopBar
+import com.markusw.chatgptapp.ui.view.screens.main.composables.PromptField
 import com.orhanobut.logger.Logger
-import kotlinx.coroutines.launch
-import com.markusw.chatgptapp.ui.view.screens.main.composables.PromptField as PromptField
 
 @Composable
 fun MainScreen(
